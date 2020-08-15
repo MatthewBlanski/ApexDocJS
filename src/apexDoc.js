d1f0c0e2b68eae8e65e3692402427977ad2fff35
Matthew Blanski <mblanski@salesforce.com>
2020-08-15 12:55:56 -0500

    Incorporated classGroup into apexDoc

--- src/apexDoc.js
+++ src/apexDoc.js
@@ -1,8 +1,9 @@
-const ApexModel = require('./apexModel.js');
-const ClassModel = require('./classModel.js');
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+
+const ClassGroup = require('./classGroup.js');
+const ClassModel = require('./classModel.js');
 
 class ApexDoc {
     constructor(sourceDirectory) {
