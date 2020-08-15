8fb0afe6bce27d08db1f5f6201b77edb13188ffb
Matthew Blanski <mblanski@salesforce.com>
2020-08-15 12:55:45 -0500

    Replicated classGroup

---
+++ src/classGroup.js
@@ -0,0 +1,35 @@
+class ClassGroup {
+    constructor(strName,strContent) {
+        this.strName = strName;
+        this.strContentSource = strContent;
+    }
+
+    getName() {
+        return this.strName;
+    }
+
+    setName(strName) {
+        this.strName = strName;
+    }
+
+    getContentSource() {
+        return this.strContentSource;
+    }
+
+    setContentSource(strContent) {
+        this.strContentSource = strContent;
+    }
+
+    getContentFilename() {
+        if (this.strContentSource) {
+            let idx1 = this.strContentSource.lastIndexOf("/");
+            let idx2 = this.strContentSource.lastIndexOf(".");
+            if (idx1 != -1 && idx2 != -1) {
+                return this.strContentSource.substring(idx1 + 1, idx2);
+            }
+        }
+        return null;
+    }
+}
+
+module.exports = ClassGroup;
