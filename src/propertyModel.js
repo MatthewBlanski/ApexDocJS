04edbf8efa3d0ee9359b0bbd5ead8f43c19a0918
Matthew Blanski <mblanski@salesforce.com>
2020-08-15 13:49:37 -0500

    Added PropertyModel

---
+++ src/propertyModel.js
@@ -0,0 +1,39 @@
+const ApexModel = require('./apexModel.js');
+
+class PropertyModel extends ApexModel {
+
+    constructor() {}
+
+    setNameLine(nameLine, iLine) {
+        if (nameLine) {
+            // remove any trailing stuff after property name. { =
+            let i = nameLine.indexOf('{');
+            if (i === -1) {
+                i = nameLine.indexOf('=');
+
+            }
+            if (i === -1) {
+                i = nameLine.indexOf(';');
+            }
+            if (i >= 0) {
+                nameLine = nameLine.substring(0, i);
+            }
+
+        }
+        super.setNameLine(nameLine, iLine);
+    }
+
+    getPropertyName() {
+        let nameLine = getNameLine().trim();
+        if (nameLine && nameLine.length > 0) {
+            const lastindex = nameLine.lastIndexOf(" ");
+            if (lastindex >= 0) {
+                const propertyName = nameLine.substring(lastindex + 1);
+                return propertyName;
+            }
+        }
+        return "";
+    }
+}
+
+module.exports = PropertyModel;
