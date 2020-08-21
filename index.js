fa4f99798ab2cb82472088d36144cabda01b89d5
Matthew Blanski <mblanski@salesforce.com>
2020-08-21 13:23:41 -0500

    Moved index to the root directory

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const ApexDoc = require('./apexDoc');
-const ApexDocsJsonParser = require('./config/apexDocsJsonParser');
+const ApexDoc = require('./src/apexDoc');
+const ApexDocsJsonParser = require('./src/config/apexDocsJsonParser');
 
 if(!process.argv[2]) {
     console.log('You have to provide a path to the repo!')
